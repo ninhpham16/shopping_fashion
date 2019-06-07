cb7f8f9338f682e6cf4af857b3e33e42591eadb7
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-06-07 11:26:55 +0700

    create-model-user

--- app/controllers/manager/static_pages_controller.rb
+++ app/controllers/manager/static_pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Manager
   class StaticPagesController < Manager::BaseController
     def index; end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { maximum: 50 }
-  validates :full_name, presence: true, length: { maximum: 255}
-  validates :phone_number, presence: true, length: { minimum: 10}
-  validates :address, presence: true, length: {maximum: 50}
-  
+  validates :full_name, presence: true, length: { maximum: 255 }
+  validates :phone_number, presence: true, length: { minimum: 10 }
+  validates :address, presence: true, length: { maximum: 50 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
