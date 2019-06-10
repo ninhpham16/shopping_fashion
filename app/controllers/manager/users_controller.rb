96173153df48d498f0fec933b1a6858e4d37967f
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-06-10 11:26:32 +0700

    Show-list-user

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 class Manager::UsersController < Manager::BaseController
-  # before_action :logged_in_user, only: [:index, :edit, :update]
 
   def index
-    @users = User.all.page(params[:page]).per(10)
-  end
-
-  def show
-    @user = User.find(params[:id])
+    @users = User.page params[:page]
   end
 end
