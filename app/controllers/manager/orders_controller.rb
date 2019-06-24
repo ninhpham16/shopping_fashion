e13a44e39c87bb56d3b1465d047fe5e40856810c
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-06-24 16:27:12 +0700

    admin_show_detail_order

--- app/controllers/manager/orders_controller.rb
+++ app/controllers/manager/orders_controller.rb
@@ -5,5 +5,16 @@ module Manager
     def index
       @orders = Order.page(params[:page])
     end
+
+    def show
+      @order = Order.find(params[:id])
+    end
+
+    def destroy
+      @order = Order.find(params[:id])
+      @order.destroy
+      flash[:success] = "Order Deleted"
+      redirect_to manager_orders_path
+    end
   end
 end
