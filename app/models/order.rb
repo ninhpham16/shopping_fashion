8ec1d88083db81fa8478cc3d4365d7746407bcd7
ninhpham16 <50564007+ninhpham16@users.noreply.github.com>
2019-06-24 13:29:44 +0700

    admin_show_list_order (#26)
    * admin_show_list_order
    
    * edit
    
    * admin-sho-list-order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  enum status: %i[padding complete]
+  enum status: %i[Pending Ordered Closed Complete]
   belongs_to :user
 
   has_many :order_items
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,39 @@
-puts 'create users'
-user = User.create!(user_name: "NinhPham",
-                    email: "ninhpham@gmail.com",
-                    password:    "123456",
-                    password_confirmation: "123456",
-                    full_name: "Pham Ngoc Ninh",
-                    phone_number: "0964980884",
-                    address: "Thai Binh"
-                   )
+ puts 'create users'
+ user = User.create!(user_name: "NinhPham",
+                     email: "ninhpham@gmail.com",
+                     password:    "123456",
+                     password_confirmation: "123456",
+                     full_name: "Georgio Armani",
+                     phone_number: "0964980884",
+                     address: "Thai Binh"
+                    )
 
 50.times do |n|
   user_name  = Faker::Name.name
   email = "ex-#{n+1}@gmail.com"
   password = "123456"
-  User.create!(user_name:  user_name,
-              email: email,
-              password: password,
-              full_name: "Pham Ngoc Ninh",
-              phone_number: "0964980884",
-              address: "Thai Binh"
+  User.create!(user_name: user_name,
+               email: email,
+               password: password,
+               full_name: "Georgio Armani",
+               phone_number: "0964980884",
+               address: "Thai Binh"
               )
- end
+  end
+  
+50.times do |n|
+  user = User.all.to_a.sample
+  full_name = User.pluck(:full_name).to_a.sample
+  address = Faker::Address.street_address
+  phone_number = Faker::PhoneNumber.phone_number
+  status = 0
+  Order.create!(user: user,
+                full_name: full_name,
+                address: address,
+                phone_number:  phone_number,
+                status: status)
+  end 
+
 
 admin = Admin.create!(user_name: "BinhPham",
                       email: "binh@gmail.com",
